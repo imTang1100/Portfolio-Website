577e863697c8079a2d0f39e53fae2c4650af81b5
imTang1100 <tangyifan.arch@gmail.com>
2024-01-13 11:44:22 -0500

    1_13_update_imagine

--- script.js
+++ script.js
@@ -4,3 +4,24 @@ function toggleMenu() {
   menu.classList.toggle("open")
   icon.classList.toggle("open")
 }
+
+document.addEventListener('DOMContentLoaded', function () {
+  var aboutPics = document.querySelectorAll('.about-pic');
+  var tooltips = document.querySelectorAll('.js-tooltip');
+
+  aboutPics.forEach(function(aboutPic, index) {
+      aboutPic.addEventListener('mousemove', function (event) {
+          // Show the tooltip corresponding to this .about-pic
+          var tooltip = tooltips[index];
+          tooltip.style.display = 'block';
+          tooltip.style.left = (event.clientX + 5) + 'px';
+          tooltip.style.top = (event.clientY + 5) + 'px';
+      });
+
+      aboutPic.addEventListener('mouseout', function () {
+          // Hide the tooltip corresponding to this .about-pic
+          var tooltip = tooltips[index];
+          tooltip.style.display = 'none';
+      });
+  });
+});
